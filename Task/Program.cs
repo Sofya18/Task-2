7d400f204869987ed648249e54facfaafdd388c3
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-04 00:25:00 +0300

    Task №2

---
+++ Task/Program.cs
@@ -0,0 +1,21 @@
+﻿// Task 2
+
+Console.Clear();
+Console.WriteLine("Введите 2 числа и мы скажем, какое из них больше");
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a < b)
+{Console.WriteLine("Второе число больше первого ");
+
+}
+else if (a > b)
+{Console.WriteLine("Первое число больше второго");  
+
+}
+else if (a == b)
+{Console.WriteLine("Оба числа равны");  
+
+}
